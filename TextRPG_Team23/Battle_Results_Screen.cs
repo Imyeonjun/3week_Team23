0f61ef3da0fbe7d904c59a8b03686ef3f63e581d
dlckdtjs <117832301+dlckdtjs@users.noreply.github.com>
2025-04-22 14:36:40 +0900

    수정사항입니다

--- TextRPG_Team23/Battle_Results_Screen.cs
+++ TextRPG_Team23/Battle_Results_Screen.cs
@@ -6,103 +6,110 @@ namespace Battle_Results_Screen
 {
 
 
-    public static class BattleResult
-
+    public class BattleResult
     {
-        public static List<string> monster = new List<string>();
-
-
-
-         public static void BattleResultUI()
+        public void BattleResultUI(Player player, bool Victory, int dungeonMonsterCount, int dungeonExp, List<Item> droppedItems)
         {
+            // 적용 전 
+            int oldLevel = player.Level;
+            int oldExp = player.Exp;
+            int totalExp = oldExp + dungeonExp;
 
-            bool bol = true;
-            int dungeonHp = 50;
-            int playerHp = 100;
-            string playerName = "chan";
-
-            int[] dungeonMonster = { 1, 2, 3, 4, 5, 6, 7, 8 };
-
-            int[] monsterLevels = { 2, 3, 5 };
-
-
-            int sumMonsterLevel = monsterLevels.Sum();
-            
-            int dungeonExp = sumMonsterLevel;
-
-            int playerLevel = 1;
-            int playerExp = 8;
-            int totalExp = playerExp + dungeonExp;
-            
-            int remaining;
-
-
-
-            if (bol)
+            if (Victory)
             {
+                Console.Clear();
                 Console.WriteLine("Battle!! - Result");
-                Console.WriteLine("Victory");
+                Console.WriteLine("Victory!");
+
+
+
+                // 새 레벨, 경험치 계산
+                int newLevel = LevelUp.GetNewLevel(oldLevel, totalExp, out int remainingExp);
+
+                //플레이어 정보 업데이트
+                player.Level = newLevel;
+                player.Exp = remainingExp;
+
+
+                Console.WriteLine($"던전에서 몬스터 {dungeonMonsterCount}마리를 잡았습니다.");
+
+                Console.WriteLine("\n[캐릭터 정보]");
+
+                Console.WriteLine($"Lv. {oldLevel} {player.Name} → Lv. {newLevel} {player.Name}");
+
+                Console.WriteLine($"EXP: {oldExp} → {remainingExp}");
+
+                Console.WriteLine($"HP: {player.MaxHp} → {player.CurrentHp}");
+
+
+                Console.WriteLine("[획득 아이템]");
 
 
 
 
-                Console.WriteLine($"던전에서 몬스터 {dungeonMonster.Length}마리를 잡았습니다.");  //던전의 몬스터 정보, 던전 몬스터의 정보를 세어서 적용
+
+                foreach (var item in droppedItems)
+                {
+                    Console.WriteLine($"{item.Name} -{item.Value}");
+                    
+                }
 
 
-                Console.WriteLine("[캐릭터 정보]");
+                Console.WriteLine("\n0. 다음");
+                Console.Write(">> ");
+
+                int choiceNumber = CheckInput(0, 0);
+                switch (choiceNumber)
+                {
+                    case 1:
+
+                        break;
+
+
+                }
+            }
+            else 
+            {
+                Console.Clear();
+                Console.WriteLine("You Lose");
 
                 
+                Console.WriteLine($"Lv. {oldLevel} {player.Name}");
+                Console.WriteLine($"{player.MaxHp} → 0");
 
-
-
-                Console.WriteLine($"Lv. {playerLevel}. {playerName} ->  {LevelUp.GetNewLevel(playerLevel,totalExp, out remaining)}. {playerName}");   // 플레이어 네임 필요, 레벨 클래스 만들기  LevelUp
-                playerExp = remaining;
-                Console.WriteLine(playerExp);
-                playerLevel = LevelUp.GetNewLevel(playerLevel, totalExp, out remaining);
-                Console.WriteLine(playerLevel);
-
-
-
-
-                Console.WriteLine($"HP {playerHp} -> {dungeonHp}");     // 현재 hp 를 보여주고 던전 이후 hp 를 보여준다. 
-                playerHp = dungeonHp;
-                Console.WriteLine(playerHp);
-
-
-
-
-                Console.WriteLine($"exp {playerExp - dungeonExp} -> {playerExp} ");   // 몬스터 레벨에 대한 정보가 필료하고, 현재 플레이어 exp 와 던전 이후 플레이어 exp가 필요 하다. 
-
-
-                Console.WriteLine("\n[획득 아이템]");
-
-                Console.WriteLine("500 Gold");                                                // 몬스터의 드롭 아이템에 대한 정보가 필요 하거나 던전 드롭 아이템에 대한 정보 필요 
-                Console.WriteLine("포션 - 1");
-                Console.WriteLine("낡은검 - 1");
-
-                Console.WriteLine("0. 다음");                                                 // 메인 스크린으로 연결한다. 
-                Console.WriteLine(">>");
-                Console.ReadLine();
-            }
-
-
-
-            else
-            {
-
-                Console.WriteLine("You Lose\n");
-                Console.WriteLine($"{playerLevel} .{playerName}");
-                Console.WriteLine($"HP 100 -> 0");
-                Console.WriteLine("\n0. 다음");                           // 엔딩 크레딧 으로  연결 
+                Console.WriteLine("0. 다음");                        // 엔딩 크레딧으로 연결
                 Console.Write(">> ");
-                Console.ReadLine();
+                
+                int choiceNumber = CheckInput(0, 0);
+                switch (choiceNumber)
+                {
+                    case 1:
+
+                        break;
+
+
+                }
+
 
             }
+        }
 
-
+        static int CheckInput(int min, int max)
+        {
+            int result;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out result);
+                if (isNumber)
+                {
+                    if (result >= min && result <= max)
+                        return result;
+                }
+                Console.WriteLine("잘못된 입력입니다!!!!");
+            }
         }
     }
-
 
     public static class LevelUp
     {
@@ -132,18 +139,93 @@ namespace Battle_Results_Screen
         }
 
         // 레벨업 계산: 새 레벨과 남은 경험치를 반환     
-        public static int GetNewLevel(int currentLevel, int totalExp, out int aExp)  //totalExp =현재 경험치+ 던전에서 받은 경험치 
+        public static int GetNewLevel(int currentLevel, int totalExp, out int remainingExp)
         {
-            while (totalExp >= GetRequiredExp(currentLevel))
+            int exp = totalExp;
+            int level = currentLevel;
+
+            while (exp >= GetRequiredExp(level))
             {
-                
-                currentLevel++;
+                exp -= GetRequiredExp(level);
+                level++;
             }
 
-            aExp = totalExp;
-            return currentLevel;
+            remainingExp = exp;
+            return level;
         }
     }
+
+    //class LevelUp2
+    //{
+    //    public static int GetRequiredExp(int level)
+    //    {
+    //        if (level == 1)
+    //        {
+    //            return 10;
+    //        }
+    //        else if (level == 2)
+    //        {
+    //            return 35;
+    //        }
+    //        else if (level == 3)
+    //        {
+    //            return 65;
+    //        }
+    //        else if (level == 4)
+    //        {
+    //            return 100;
+    //        }
+    //        else
+    //        {
+    //            return int.MaxValue;
+    //        }
+    //    }
+    //    public void firstSetting()
+    //    {
+    //        Level = 1;
+
+    //    }
+    //    public int Level { get; private set; }
+
+    //    private int exp;
+
+    //    public int Exp
+    //    {
+    //        get { return exp; }
+    //        set
+    //        {
+
+    //            if (value > 0)
+    //            {
+    //                exp += value;
+
+    //                if (Level < 10)
+    //                {
+    //                    while (exp >= GetRequiredExp(Level) && Level < 10 ) 
+    //                    {
+
+    //                        exp -= GetRequiredExp(Level);
+    //                        Level++;
+
+    //                    }
+    //                    if (Level == 10)
+    //                    {
+    //                        Console.WriteLine("최대 레벨입니다.");
+    //                        exp = 0;
+    //                    }
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("최대 레벨입니다.");
+    //                }
+    //            }
+
+
+    //        }
+    //    }
+
+    //}
+
 
 
     internal class Program
@@ -156,4 +238,18 @@ namespace Battle_Results_Screen
 
 
 
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
