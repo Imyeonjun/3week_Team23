3e53e168412992e544c37adc3eab90726bf6e7ce
ChaWooJin <chawoojin06@gmail.com>
2025-04-23 19:46:45 +0900

    대장간

---
+++ TextRPG_Team23/01.Forge.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TextRPG_Team23
+{
+    public struct Obj
+    {
+        public int x;
+        public int y;
+        public string button;
+        public Obj(int _x, int _y, string _button)
+        {
+            x = _x;
+            y = _y;
+            button = _button;
+        }
+        public void ObjPos(int _x, int _y, string image)
+        {
+            Console.SetCursorPosition(_x, _y);
+            Console.WriteLine(image);
+        }
+        
+    }
+    internal class Forge : Inventory
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            //Forge forge = new Forge();
+            //forge.Selection();
+
+            Inn inn = new Inn();
+            inn.Selection();
+        }
+        UpGrade upGrade = new UpGrade();
+
+        Obj obj = new Obj(1, 1, "▶");
+        public void Selection()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("- Welcome - \n   1. 강화 \n   2. 제작 \n   3. 수리");
+
+                Console.SetCursorPosition(obj.x, obj.y);
+                Console.WriteLine(obj.button);
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (obj.y > 1)
+                        {
+                            obj.y--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (obj.y < 3)
+                        {
+                            obj.y++;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        switch (obj.y)
+                        {
+                            case 1:
+                                upGrade.ItemSelection();
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    
+}
+//아이템 강화
+//돈, 다른 재화로 착용가능한 장비를 강화
+//강화 래밸은 최대 10까지
+//1lv100%, 2lv90%,  3lv80%
+//4lv70%, 5lv60%,  6lv50%
+//(강화 단계 하락)7lv40%, 8lv30%, 9lv20%
+//(장비 파괴?)10lv10%
+//10% 강화2단계 상승
