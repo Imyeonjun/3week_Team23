444f156fcbe46bb04a66c21a9f494d796d2d1809
ChaWooJin <chawoojin06@gmail.com>
2025-04-24 18:57:41 +0900

    Update oojin.Temple.cs

--- TextRPG_Team23/oojin.Temple.cs
+++ TextRPG_Team23/oojin.Temple.cs
@@ -14,20 +14,29 @@ namespace TextRPG_Team23
             Console.WriteLine("1. 헌금 2. 전직 3. 나가기");
 
             int.TryParse(Console.ReadLine(), out int input);
-            switch(input)
-            {// 헌금만 가능하며 돈이 싸이면 버프의 효과가 상승 세례(버프) 축복(상태이상해제)
-                case 1:
-                    Console.WriteLine("1. 헌금 2. 세례 3. 축복");
+            if (input <= 3)
+            {
+                switch (input)
+                {// 헌금만 가능하며 돈이 싸이면 버프의 효과가 상승 세례(버프) 축복(상태이상해제)
+                    case 1:
+                        Console.WriteLine("1. 헌금 2. 세례 3. 축복");
 
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    Console.WriteLine("잘못 입력하였습니다.");
-                    break;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("잘못 입력하였습니다.");
+                        break;
+                }
             }
+            else
+            {
+                Console.WriteLine("잘못 입력하였습니다.");
+                return;
+            }
+
         }
     }
 }
