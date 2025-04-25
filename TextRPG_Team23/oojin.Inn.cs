d519810f07f89f69b19c55b7e1ea50af41da264b
ChaWooJin <chawoojin06@gmail.com>
2025-04-25 21:47:27 +0900

    Update oojin.Inn.cs

--- TextRPG_Team23/oojin.Inn.cs
+++ TextRPG_Team23/oojin.Inn.cs
@@ -72,7 +72,9 @@ namespace TextRPG_Team23
             {
                 player.CurrentHp = player.MaxHp;
             }
+            //if(player.CurrentHp == )
             Console.WriteLine($"하루가 지나 체력을 회복했습니다. 현재 체력 : {player.CurrentHp}");
+            
         }
         private void QuestInfo(QuestMenu questMenu, Player player)
         {
