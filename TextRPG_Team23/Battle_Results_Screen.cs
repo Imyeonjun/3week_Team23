f8496d82ff699d8ba2937a1d33a223861bffc235
dlckdtjs <117832301+dlckdtjs@users.noreply.github.com>
2025-04-25 17:50:54 +0900

    수정
    수정

--- TextRPG_Team23/Battle_Results_Screen.cs
+++ TextRPG_Team23/Battle_Results_Screen.cs
@@ -126,21 +126,7 @@ namespace TextRPG_Team23
             
         }
 
-        static int CheckInput(int min, int max)
-        {
-            int result;
-            while (true)
-            {
-                string input = Console.ReadLine();
-                bool isNumber = int.TryParse(input, out result);
-                if (isNumber)
-                {
-                    if (result >= min && result <= max)
-                        return result;
-                }
-                Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
-            }
-        }
+       
     }
 
     public static class LevelUp
