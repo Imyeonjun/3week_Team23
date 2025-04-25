d68f38da86e75a3fec8f04a380e44b32194589da
ChaWooJin <chawoojin06@gmail.com>
2025-04-25 11:40:49 +0900

    Update oojin.Forge.cs

--- TextRPG_Team23/oojin.Forge.cs
+++ TextRPG_Team23/oojin.Forge.cs
@@ -18,20 +18,34 @@ namespace TextRPG_Team23
             while (true)
             {
                 //Console.Clear();
-                Console.WriteLine("- @@대장간에 어서오세요 - \n   1. 강화 \n   2. 제작 \n   3. 수리");
+                Console.WriteLine("- @@대장간에 어서오세요 - \n   1. 강화 \n   2. 제작 \n   3. 수리 0. 나가기");
                 int.TryParse(Console.ReadLine(), out int input);
-                switch (input)
+
+                if (input > 0 && input <= 2)
                 {
-                    case 1:
-                        //Console.Clear();
-                        upGrade.ItemSelection();
-                        return;
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        Repair();
-                        break;
+                    switch (input)
+                    {
+                        case 1:
+                            //Console.Clear();
+                            upGrade.ItemSelection();
+                            return;
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            Repair();
+                            break;
+                    }
+                }
+                else if (input ==  0)
+                {
+                    Console.WriteLine("마을로 돌아갑니다.");
+                    Console.WriteLine("Enter키를 눌러주세요");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("잘못 입력했습니다, 다시 입력해 주세요\n");
                 }
             }
         }
@@ -45,7 +59,7 @@ namespace TextRPG_Team23
 
             for (int i = 0; i < items.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. {items[i].Item.Name} (내구도: {items[i].Item.Durability}/{items[i].Item.MaxDurability})");
+                Console.WriteLine($"{i + 1}. {items[i].Item.Name} (내구도: {items[i].Item.Durability}/{items[i].Item.MaxDurability})\n");
             }
 
             do
