66d860956f0914c59eb58070279150d4772b3998
ChaWooJin <chawoojin06@gmail.com>
2025-04-25 20:41:16 +0900

    Update oojin.UpGrade.cs

--- TextRPG_Team23/oojin.UpGrade.cs
+++ TextRPG_Team23/oojin.UpGrade.cs
@@ -40,80 +40,54 @@ namespace TextRPG_Team23
             {
                 //Console.Clear();
 
-                //Console.WriteLine(" == 계속 하시겠습니까? == ");
-                //Console.Write(" 1. [YES] 2. [NO] \n >>>");
-
-                int.TryParse(Console.ReadLine(), out int num);
-                if (num > 0 && num <= 2)
+                var equipmentItems = player.Inventory.Items.Where(i => i.Item is Weapon || i.Item is Clothes).ToList(); // Weapon, Clothes 아이템이 equipmentitems에 저장
+                if (equipmentItems.Count > 0) // 아이템이 하나 이상 들어있다면
                 {
-                    if (num == 1)
+
+                    Console.WriteLine("\n == 강화 할 장비를 (번호)선택하세요 == ");
+                    int i = 0;
+                    foreach (var itemList in equipmentItems)
                     {
-                        var equipmentItems = player.Inventory.Items.Where(i => i.Item is Weapon || i.Item is Clothes).ToList(); // Weapon, Clothes 아이템이 equipmentitems에 저장
-                        if (equipmentItems.Count > 0) // 아이템이 하나 이상 들어있다면
-                        {
+                        bool isEquipped = Array.Exists(player.Inventory.Slots, slots => slots == itemList.Item); // 배열을 돌면서 slots변수에 들어있는 아이템이 있는지 검사해서 bool값을 반환
+                        string prefix = isEquipped ? "[E] " : ""; // true라면 들어있으면 [E]를 넣어줌
 
-                            Console.WriteLine(" == 강화 할 장비를 (번호)선택하세요 == ");
-                            int i = 0;
-                            foreach (var itemList in equipmentItems)
-                            {
-                                bool isEquipped = Array.Exists(player.Inventory.Slots, slots => slots == itemList.Item); // 배열을 돌면서 slots변수에 들어있는 아이템이 있는지 검사해서 bool값을 반환
-                                string prefix = isEquipped ? "[E] " : ""; // true라면 들어있으면 [E]를 넣어줌
-
-                                Console.WriteLine($"{i + 1}. {prefix}{itemList}");
-                                i++;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("강화할 장비 아이템이 없습니다.");
-                            Console.WriteLine("아무키를 누르고 Enter로 나가기");
-                            return;
-                        }
-
-                        int.TryParse(Console.ReadLine(), out int input);
-                        for (int i = 0; i < equipmentItems.Count; i++)
-                        {
-                            if (input >= 1 && input <= equipmentItems.Count)
-                            {
-                                if (input == i + 1) // 입력한 숫자와 i값이 같다면
-                                {
-                                    Console.WriteLine();
-
-                                    selectedItem = equipmentItems[i].Item; // 입력한 숫자와 같은 인덱스에 있는 아이템을 selecteditem에 저장
-                                    Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) 선택되었습니다.\n");
-                                    //Console.Clear();
-                                    ItemUpgrade(forge);
-                                    break;
-                                }
-                            }
-                            else
-                            {
-                                BranchManager.ErrorMessage("잘못된 번호입니다,  Enter를 누른 후 다시 입력해주세요.");
-                                //Console.WriteLine("잘못된 번호입니다");
-                                break;
-                            }
-                        }
-                    }
-                    else if(num == 2)
-                    {
-                        forge.Selection(forge);
-
-                        return;
-                    }
-                    else
-                    {
-                        BranchManager.ErrorMessage("잘못 입력했습니다, Enter를 누른 후 다시 입력해주세요");
-                        //Console.WriteLine("잘못 입력했습니다.");
-                        ItemUpgrade(forge);
+                        Console.WriteLine($"{i + 1}. {prefix}{itemList} \n>>> ");
+                        i++;
                     }
                 }
                 else
                 {
-                    BranchManager.ErrorMessage("잘못 입력했습니다, Enter를 누른 후 다시 입력해주세요");
-                    //Console.WriteLine("잘못 입력했습니다.");
-                    ItemUpgrade(forge);
+                    Console.WriteLine("강화할 장비 아이템이 없습니다.");
+                    Console.WriteLine("아무키를 누르고 Enter로 나가기");
+                    return;
                 }
-                
+
+                int.TryParse(Console.ReadLine(), out int input);
+                for (int i = 0; i < equipmentItems.Count; i++)
+                {
+                    if (input >= 1 && input <= equipmentItems.Count)
+                    {
+                        if (input == i + 1) // 입력한 숫자와 i값이 같다면
+                        {
+                            selectedItem = equipmentItems[i].Item; // 입력한 숫자와 같은 인덱스에 있는 아이템을 selecteditem에 저장
+                            Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) 선택되었습니다.\n");
+                            if (selectedItem.Upgrade == 10)
+                            {
+                                Console.WriteLine("더 이상 강화할 수 없습니다, 다른 아이템을 골라 주세요\n");
+                                return;
+                            }
+                            //Console.Clear();
+                            ItemUpgrade(forge);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        BranchManager.ErrorMessage("잘못된 번호입니다,  Enter를 누른 후 다시 입력해주세요.");
+                        //Console.WriteLine("잘못된 번호입니다");
+                        break;
+                    }
+                }
             }
         }
         private void ItemUpgrade(Forge forge)
@@ -121,9 +95,12 @@ namespace TextRPG_Team23
             while (true)
             {
                 //Console.Clear();
-
+                if(selectedItem == null)
+                {
+                    return;
+                }
                 Console.WriteLine($" == 아이템\"{selectedItem.Name}\" 을(를) 강화하시겠습니까? == ");
-                Console.WriteLine(" 1. [YES] 2. [NO]");
+                Console.Write(" 1. [YES] 2. [NO] \n>>> ");
 
                 int.TryParse(Console.ReadLine(), out int input);
                 if (input > 0 && input <= 2)
@@ -158,7 +135,7 @@ namespace TextRPG_Team23
             {
                 selectedItem.Upgrade++;
                 Console.WriteLine(" == Success == ");
-                Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) + {selectedItem.Upgrade} 되었습니다.\n");
+                Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) {selectedItem.Upgrade} + {selectedItem.Name} 되었습니다.\n");
             }
             else // 강화에 실패
             {
@@ -167,31 +144,17 @@ namespace TextRPG_Team23
                 if (selectedItem.Upgrade >= 6 && selectedItem.Upgrade <= 8)
                 {
                     //selectedItem.Upgrade--;
-                    Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) + {selectedItem.Upgrade} 되었습니다.\n");
+                    Console.WriteLine($"아이템 \"{selectedItem.Name}\" 이(가) \"+ {selectedItem.Upgrade}\" 되었습니다.\n");
                 }
                 else if (selectedItem.Upgrade == 9)
                 {
                     
-                    Console.WriteLine($"아이템 {selectedItem}이(가) 파괴되었습니다.");
+                    Console.WriteLine($"아이템 \"{selectedItem.Name}\"이(가) 파괴되었습니다.\n");
                     player.Inventory.RemoveItem(selectedItem);
-                    forge.Selection(forge);
+                    selectedItem = null;
 
                     return;
                 }
-                //else if (selectedItem.Upgrade == 10)
-                //{
-                //    Console.WriteLine("더 이상 강화할 수 없습니다.");
-                //    a = false;
-                //    forge.Selection(forge);
-                    
-                //    return;
-                //}
-                else
-                {
-                    return;
-                }
-                Console.WriteLine("Enter를 눌러서 계속...");
-                Console.ReadLine();
             }
         }
     }
